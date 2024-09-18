769987165a83b467b1589c3889bb58ed02963ffc
nelil21 <nelilamaani21@gmail.com>
2024-09-18 11:32:24 +0700

    tes

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/nelil.amaani/mentalhealthtracker", "https://pbp.cs.ui.ac.id/nelil.amaani/mentalhealthtracker"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/nelil-amaani/mentalhealthtracker", "https://pbp.cs.ui.ac.id/nelil-amaani/mentalhealthtracker"]
