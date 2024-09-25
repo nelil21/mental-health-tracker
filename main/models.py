671a2e5acc5b1285a1ab06ee5204d9aecad9a960
nelil21 <nelilamaani21@gmail.com>
2024-09-25 14:03:13 +0700

    menyelesaikan tutorial 4

---
+++ main/models.py
@@ -0,0 +1,16 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
